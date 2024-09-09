72fd07475eedfb717c60d526816516cf61370cab
vitorboos <vitor.teixeira@sptech.school>
2024-09-09 17:57:24 -0300

    Fix: HashTable changed to List

--- inventory.manager/src/main/java/com/velas/vivene/inventory/manager/repository/VelaRepository.java
+++ inventory.manager/src/main/java/com/velas/vivene/inventory/manager/repository/VelaRepository.java
@@ -3,8 +3,9 @@ package com.velas.vivene.inventory.manager.repository;
 import com.velas.vivene.inventory.manager.entity.Vela;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VelaRepository extends JpaRepository<Vela, Integer> {
-    Optional<Vela> findByNome(String nome);
+    List<Vela> findAllByNomeIn(List<String> nomes);
 }
