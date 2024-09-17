4169f283882e06991ddb338ddd522d829ca05582
vitorboos <vitor.teixeira@sptech.school>
2024-09-17 18:45:00 -0300

    Feat: Detalhamento do swagger ffeito

--- inventory.manager/src/main/java/com/velas/vivene/inventory/manager/controller/MetaController.java
+++ inventory.manager/src/main/java/com/velas/vivene/inventory/manager/controller/MetaController.java
@@ -1,11 +1,14 @@
 package com.velas.vivene.inventory.manager.controller;
 
-import com.velas.vivene.inventory.manager.dto.meta.MetaMapper;
 import com.velas.vivene.inventory.manager.dto.meta.MetaRequestDto;
 import com.velas.vivene.inventory.manager.dto.meta.MetaResponseDto;
 import com.velas.vivene.inventory.manager.dto.ultimametaseismeses.UltimaMetaSeisMesesResponse;
 import com.velas.vivene.inventory.manager.service.MetaService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,43 +27,111 @@ public class MetaController {
 
     private final MetaService metaService;
 
-    @Operation(summary = "Cria uma nova meta")
     @PostMapping
+    @Operation(summary = "Cria uma nova meta", description = """
+            # Criar meta
+            ---
+            Esse endpoint cria uma nova meta
+            ---
+            Nota:
+            Todos os atributos são obrigatórios
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Meta criada com sucesso.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MetaResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Campos da meta estão incorretos.",
+                    content = @Content(mediaType = "application/json", schema = @Schema()))})
     public ResponseEntity<MetaResponseDto> createMeta(@Valid @RequestBody MetaRequestDto metaRequestDto) {
         MetaResponseDto metaResponseDto = metaService.createMeta(metaRequestDto);
         return new ResponseEntity<>(metaResponseDto, HttpStatus.CREATED);
     }
 
-    @Operation(summary = "Atualiza uma meta existente")
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza uma meta existente", description = """
+            # Atualizar meta
+            ---
+            Esse endpoint atualiza uma meta existente
+            ---
+            Nota:
+            Todos os atributos são obrigatórios
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Meta atualizada com sucesso.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MetaResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Campos da meta estão incorretos.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema())}),
+            @ApiResponse(responseCode = "404", description = "Meta não encontrada.",
+                    content = @Content(mediaType = "application/json", schema = @Schema()))})
     public ResponseEntity<MetaResponseDto> updateMeta(@PathVariable Integer id, @Valid @RequestBody MetaRequestDto metaRequestDto) {
         MetaResponseDto metaResponseDto = metaService.updateMeta(id, metaRequestDto);
         return new ResponseEntity<>(metaResponseDto, HttpStatus.OK);
     }
 
-    @Operation(summary = "Deleta uma meta existente")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deleta uma meta existente", description = """
+            # Deletar meta
+            ---
+            Esse endpoint deleta uma meta existente
+            ---
+            Nota:
+            A meta deve existir no banco de dados
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Meta deletada com sucesso.",
+                    content = @Content(mediaType = "application/json", schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Meta não encontrada.",
+                    content = @Content(mediaType = "application/json", schema = @Schema()))})
     public ResponseEntity<Void> deleteMeta(@PathVariable Integer id) {
         metaService.deletarMeta(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Operation(summary = "Busca todas as metas")
     @GetMapping
+    @Operation(summary = "Busca todas as metas", description = """
+            # Busca todas as metas
+            ---
+            Esse endpoint busca todas as metas existentes
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Metas buscadas com sucesso.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MetaResponseDto.class))}),
+            @ApiResponse(responseCode = "204", description = "Nenhuma meta encontrada no banco de dados.",
+                    content = @Content(mediaType = "application/json", schema = @Schema()))})
     public ResponseEntity<List<MetaResponseDto>> getAllMetas() {
         List<MetaResponseDto> metaResponseDtos = metaService.getAllMetas();
         return new ResponseEntity<>(metaResponseDtos, HttpStatus.OK);
     }
 
-    @Operation(summary = "Busca uma meta pelo ID")
     @GetMapping("/{id}")
+    @Operation(summary = "Busca uma meta pelo ID", description = """
+            # Busca meta pelo ID
+            ---
+            Esse endpoint busca uma meta existente pelo ID
+            ---
+            Nota:
+            O ID é obrigatório
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Meta encontrada com sucesso.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MetaResponseDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Meta não encontrada.",
+                    content = @Content(mediaType = "application/json", schema = @Schema()))})
     public ResponseEntity<MetaResponseDto> getMetaById(@PathVariable Integer id) {
         MetaResponseDto metaResponseDto = metaService.getMetaById(id);
         return new ResponseEntity<>(metaResponseDto, HttpStatus.OK);
     }
 
-    @Operation(summary = "Busca a ultima meta de cada mes, num período de seis meses")
     @GetMapping("/ultima-meta")
+    @Operation(summary = "Busca a última meta de cada mês, num período de seis meses", description = """
+            # Busca última meta de cada mês
+            ---
+            Esse endpoint busca a última meta de cada mês nos últimos seis meses
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Metas buscadas com sucesso.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UltimaMetaSeisMesesResponse.class))}),
+            @ApiResponse(responseCode = "204", description = "Nenhuma meta encontrada no período especificado.",
+                    content = @Content(mediaType = "application/json", schema = @Schema()))})
     public ResponseEntity<List<UltimaMetaSeisMesesResponse>> getUltimaMetaSeisMeses() {
         List<UltimaMetaSeisMesesResponse> responses = metaService.getUltimaMetaSeisMeses();
         return new ResponseEntity<>(responses, HttpStatus.OK);
--- inventory.manager/src/main/java/com/velas/vivene/inventory/manager/controller/VelaController.java
+++ inventory.manager/src/main/java/com/velas/vivene/inventory/manager/controller/VelaController.java
@@ -6,6 +6,10 @@ import com.velas.vivene.inventory.manager.dto.vela.VelaResponseDto;
 import com.velas.vivene.inventory.manager.dto.velamaisvendida.VelaMaisVendidaResponse;
 import com.velas.vivene.inventory.manager.service.VelaService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,57 +27,158 @@ public class VelaController {
 
     private final VelaService velaService;
 
-    @Operation(summary = "Cria uma nova vela")
     @PostMapping
+    @Operation(summary = "Cria uma nova vela", description = """
+           # Criar vela
+           ---
+           Esse endpoint cria uma nova vela
+           ---
+           Nota:
+           Todos os atributos são obrigatórios
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Vela criada com sucesso (OK).",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VelaResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Campos de vela estão incorretos",
+            content = @Content(mediaType = "application/json", schema = @Schema()))})
     public ResponseEntity<VelaResponseDto> createVela(@Valid @RequestBody VelaRequestDto velaRequestDTO) {
         VelaResponseDto responseDTO = velaService.createVela(velaRequestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
-    @Operation(summary = "Atualiza uma vela existente")
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza uma vela existente", description = """
+           # Atualizar vela
+           ---
+           Esse endpoint atualiza uma vela existente
+           ---
+           Nota:
+           Todos os atributos são obrigatórios
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vela atualizada com sucesso (OK).",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = VelaResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Campos de vela estão incorretos.",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())})}
+    )
     public ResponseEntity<VelaResponseDto> updateVela(@PathVariable Integer id, @Valid @RequestBody VelaRequestDto velaRequestDTO) {
         VelaResponseDto responseDTO = velaService.updateVela(id, velaRequestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
-    @Operation(summary = "Deleta uma vela existente")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deleta uma vela existente", description = """
+           # Deletar vela
+           ---
+           Esse endpoint deleta uma vela existente
+           ---
+           Nota:
+           A vela deve existir no banco de dados
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Vela deletada com sucesso (OK).",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())}),
+            @ApiResponse(responseCode = "404", description = "Buffet não existe no banco de dados.",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())})})
     public ResponseEntity<Void> deleteVela(@PathVariable Integer id) {
         velaService.deleteVela(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Operation(summary = "Busca todas as velas")
     @GetMapping
+    @Operation(summary = "Busca todas as velas", description = """
+           # Busca todas as velas
+           ---
+           Esse endpoint busca todas as velas existentes
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Velas buscadas com sucesso (OK).",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = VelaResponseDto.class))}),
+            @ApiResponse(responseCode = "204", description = "Vela não existe no banco de dados.",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())})}
+    )
     public ResponseEntity<List<VelaResponseDto>> getAllVelas() {
         List<VelaResponseDto> responseDTOList = velaService.getAllVelas();
         return new ResponseEntity<>(responseDTOList, HttpStatus.OK);
     }
 
-    @Operation(summary = "Busca uma vela pelo ID")
     @GetMapping("/{id}")
+    @Operation(summary = "Busca uma vela pelo ID", description = """
+           # Busca uma vela existente pelo id
+           ---
+           Esse endpoint busca uma vela existente pelo id
+           ---
+           Nota:
+           O id é obrigatório
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vela encontrada com sucesso (OK).",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = VelaResponseDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Vela não encontrada.",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())})}
+    )
     public ResponseEntity<VelaResponseDto> getVelaById(@PathVariable Integer id) {
         VelaResponseDto responseDTO = velaService.getVelaById(id);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
-    @Operation(summary = "Buscar vela mais vendida")
     @GetMapping("/maisvendida")
+    @Operation(summary = "Buscar vela mais vendida", description = """
+           # Busca a vela mais vendida
+           ---
+           Esse endpoint busca a vela mais vendida
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vela mais vendida encontrada com sucesso (OK).",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = VelaMaisVendidaResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Vela não encontrada.",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())})}
+    )
     public ResponseEntity<List<VelaMaisVendidaResponse>> getVelaVendida() {
         List<VelaMaisVendidaResponse> responseDTO = velaService.getVelaVendida();
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
-    @Operation(summary = "Buscar as 5 velas mais vendidas")
     @GetMapping("/top-mais-vendidas")
+    @Operation(summary = "Buscar as 5 velas mais vendidas", description = """
+           # Busca as top cinco velas mais vendida
+           ---
+           Esse endpoint busca as top cinco velas mais vendida
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Top cinco velas mais vendidas encontrada com sucesso (OK).",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Top5VelasMaisVendidasResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Vela não encontrada.",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())})}
+    )
     public ResponseEntity<List<Top5VelasMaisVendidasResponse>> getTop5VelasVendidas() {
         List<Top5VelasMaisVendidasResponse> top5VelasMaisVendidasResponses = velaService.getTop5VelasVendidas();
         return new ResponseEntity<>(top5VelasMaisVendidasResponses, HttpStatus.OK);
     }
 
-    @Operation(summary = "Buscar velas pelo nome")
     @GetMapping("/buscar-vela-nome/{nome}")
+    @Operation(summary = "Buscar velas pelo nome", description = """
+           # Busca as top cinco velas mais vendida
+           ---
+           Esse endpoint busca as top cinco velas mais vendida
+           ---
+           Nota:
+           O nome da vela é obrigatório
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vela encontrada com sucesso (OK).",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Top5VelasMaisVendidasResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Vela não encontrada.",
+                    content = { @Content(mediaType = "application/json", schema = @Schema())})}
+    )
     public List<VelaResponseDto> getVelasByName(@PathVariable String nome) {
         return velaService.getVelasByName(nome);
     }
